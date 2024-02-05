794e3aa562169fa286d75ff0e5196b35483a41ff
Yahya Osman <lykan622@gmail.com>
2024-02-05 15:45:23 -0500

    Changed back middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextRequest, NextResponse } from "next/server";
 
-/* export default authMiddleware({
+export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
@@ -14,44 +13,8 @@ import { NextRequest, NextResponse } from "next/server";
     "/api/webhook/stripe",
     "/api/uploadthing",
   ],
-}); */
-
-export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-
-  const hostname = req.headers.get("host")!;
-
-  const path = url.pathname;
-
-  let subdomain = hostname.split(".")[0];
-
-  subdomain = subdomain.replace("localhost:3000", "");
-
-  // handle no subdomain or www with base path
-  if (subdomain === "www" || subdomain === "") {
-    return NextResponse.next();
-  }
-
-  // subdomains
-  if (subdomain !== "app") {
-    return NextResponse.rewrite(
-      new URL(`/users/${subdomain}${path === "/" ? "" : path}`, req.url)
-    );
-  }
-
-  return NextResponse.next();
-}
+});
 
 export const config = {
-  /* matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"], */
-  matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api routes
-     * 2. /_next (Next.js internals)
-     * 3. /_static (inside /public)
-     * 4. all root files inside /public
-     */
-    "/((?!api/|_next/|_static/|[\\w-]+\\.\\w+).*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
