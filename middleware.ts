a526d86e7a5221318a8531edcf33f345ba6c891d
Yahya Osman <lykan622@gmail.com>
2024-02-04 23:38:50 -0500

    Test the middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
 
-export default authMiddleware({
+/* export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
@@ -13,7 +14,33 @@ export default authMiddleware({
     "/api/webhook/stripe",
     "/api/uploadthing",
   ],
-});
+}); */
+
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+
+  const hostname = req.headers.get("host")!;
+
+  const path = url.pathname;
+
+  let subdomain = hostname.split(".")[0];
+
+  subdomain = subdomain.replace("localhost:3000", "");
+
+  // handle no subdomain or www with base path
+  if (subdomain === "www" || subdomain === "") {
+    return NextResponse.next();
+  }
+
+  // subdomains
+  if (subdomain !== "app") {
+    return NextResponse.rewrite(
+      new URL(`/users/${subdomain}${path === "/" ? "" : path}`, req.url)
+    );
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
